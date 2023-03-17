69b11808d79a83a34c5c1d1afc28f53869670cfb
devcom22 <mouad.elb06@gmail.com>
2023-03-17 13:00:04 -0700

    add 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabetic
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((char)i);
+	}
+	putchar('\n');
+	return (0);
+}
