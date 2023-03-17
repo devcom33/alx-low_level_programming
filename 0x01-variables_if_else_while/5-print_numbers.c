0129ac4bc82371e98cd79bae5dabae1e2c759fcc
devcom22 <mouad.elb06@gmail.com>
2023-03-17 12:52:56 -0700

    add 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabetic except e and q
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
