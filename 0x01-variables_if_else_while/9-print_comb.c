cfe11ac9fc31275aa0031548f3a18355d23de37b
devcom22 <mouad.elb06@gmail.com>
2023-03-17 16:29:43 -0700

    9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
