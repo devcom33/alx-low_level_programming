f9e2de954613c99b4b73472183423655fa5a1de3
devcom22 <mouad.elb06@gmail.com>
2023-03-16 18:09:58 -0700

    quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - sprintf and that piece of art,
+ * Return: 1
+ */
+
+int main(void)
+{
+	sprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+
+}
