836fed69f9b69ce7438d6beb509ebd645cf7d855
devcom22 <mouad.elb06@gmail.com>
2023-03-16 17:19:40 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Puts "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
 int main(void)
 {
+	/*
+	 * main function
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
