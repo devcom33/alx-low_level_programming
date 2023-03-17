cc54b12f4831c6bd438ff11e47e696cedbcd8190
devcom22 <mouad.elb06@gmail.com>
2023-03-17 11:26:55 -0700

    2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabetic
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
