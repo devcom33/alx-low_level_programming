1b40fbd700ee0a6a56768f10e0adf13361372e73
devcom22 <mouad.elb06@gmail.com>
2023-03-16 17:27:53 -0700

    printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Puts with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
