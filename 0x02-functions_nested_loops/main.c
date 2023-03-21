d388510277ff8604aa6c9cad3eb426cace4551fe
devcom22 <mouad.elb06@gmail.com>
2023-03-21 08:34:34 -0700

    updates and add main.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ * Return : Nothing
+ */
+
+
+void print_alphabet(void)
+{
+        char i;
+        for (i = 'a'; i <= 'z'; i++)
+        {
+                putchar(i);
+        }
+        putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-void print_alphabet(void)
-{
-	char i;
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-}
+
+void print_alphabet(void);
 #endif
