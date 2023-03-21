281ff8efc7be6a9d9e5a02d4eec99e8fbbbe26fe
devcom22 <mouad.elb06@gmail.com>
2023-03-21 11:41:10 -0700

    update main.h and add islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ * Return - nothing
+ */
+int _islower(int c)
+{
+	if((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
