7c68bc09757eadb2e1f6172fc088003abdccaf6c
devcom22 <mouad.elb06@gmail.com>
2023-03-21 09:28:09 -0700

    update files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,11 @@
  */
 void print_alphabet(void)
 {
-        char i;
-        for (i = 'a'; i <= 'z'; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
 }
