0e0711be4444da43ab1919cc27ab4c1420986c09
devcom22 <mouad.elb06@gmail.com>
2023-03-22 17:07:06 -0700

    update files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print last digit
+ */
+void jack_bauer(void)
+{
+	for (i = 0; i < 24; i++)
+	{
+		if (i < 10)
+		{
+			_putchar('0');
+		}
+		_putchar(i + '0');
+		_putchar(':');
+		for (j = 0; j < 60; j++)
+		{
+			if (j < 10)
+			{
+				_putchar('0');
+			}
+			_putchar(j + '0');
+		}
+	}
+}
