5c9a680205396cb2ad2e2136e1ce43e312839148
devcom22 <mouad.elb06@gmail.com>
2023-03-22 17:12:35 -0700

    update files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,21 +7,23 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
-		if (i < 10)
-		{
-			_putchar('0');
-		}
-		_putchar(i + '0');
-		_putchar(':');
 		for (j = 0; j < 60; j++)
 		{
+			if (i < 10)
+			{
+				_putchar('0');
+			}
+			_putchar(i + '0');
+			_putchar(':');
 			if (j < 10)
 			{
 				_putchar('0');
 			}
 			_putchar(j + '0');
 		}
+		_putchar('\n');
 	}
 }
