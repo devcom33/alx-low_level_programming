7e102ac4c07d9477202e6879aaf49bc9dce5775b
devcom22 <mouad.elb06@gmail.com>
2023-03-25 08:16:19 -0700

    5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			putchar(i + '0');
+		}
+		for (i = 0; i <= 4; i++)
+		{
+			putchar('1');
+			putchar(i + '0');
+		}
+		putchar('\n');
+	}
+}
