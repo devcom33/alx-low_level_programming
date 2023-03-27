28b3e9f3d950cd285abd0e660e84b29e002281a0
devcom22 <mouad.elb06@gmail.com>
2023-03-27 07:59:23 -0700

    add _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: string argument
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+}
