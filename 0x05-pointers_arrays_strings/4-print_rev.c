4362a54f131e4cd65ba0622435617d521043d28f
devcom22 <mouad.elb06@gmail.com>
2023-03-27 08:39:45 -0700

    update print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,9 +5,14 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, len = 0;
 
-	for (i = _strlen(s) - 1; i >= 0; i--)
+	while (*(s + i) != '\0')
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(*(s + i));
 	}
