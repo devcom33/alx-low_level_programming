0b7485c353100c0b5d8009668afdb918350cb42b
devcom22 <mouad.elb06@gmail.com>
2023-03-27 07:45:30 -0700

    add strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len;
+	int i = 0;
+
+	while ((s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
