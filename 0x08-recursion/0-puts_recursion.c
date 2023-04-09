220bdafdd4c9eda70d0612ebed1b89ad176707c4
devcom22 <mouad.elb06@gmail.com>
2023-04-09 08:53:13 -0700

    add 0-puts_recursion.c with code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion -  prints a string
+ * @s: string argument
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
