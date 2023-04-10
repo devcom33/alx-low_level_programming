d68ea6e14ecbee2bd602040d6e5937bfd6d7256d
devcom22 <mouad.elb06@gmail.com>
2023-04-09 17:42:04 -0700

    add code to 1-print_rev_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - len of a string
+ * @s: string argument
+ * Return: len of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return (i);
+	}
+	if (s[i] != '\0')
+	{
+		i++;
+	}
+	_strlen_recursion(s + i);
+	return (i);
+}
