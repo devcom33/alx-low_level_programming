aed2019cc1c7f60f066946309e339b785ac71303
devcom22 <mouad.elb06@gmail.com>
2023-04-15 23:29:01 -0700

    add codes to 2-strlen_recursion.c file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,9 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s)
+	if (!(*s))
 	{
-		_strlen_recursion(s + 1);
-		i++;
+		return (0);
 	}
-	return (i);
+	return (1 + _strlen_recursion(s + 1));
 }
