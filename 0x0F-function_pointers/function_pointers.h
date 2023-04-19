d3af080a890448beef5d25dd1dd4beb320c15d9e
devcom22 <mouad.elb06@gmail.com>
2023-04-19 09:43:14 -0700

    update function_pointers.h file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
+#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stddef.h>
 void print_name_uppercase(char *name);
 void print_name_as_is(char *name);
 void print_name(char *name, void (*f)(char *));
