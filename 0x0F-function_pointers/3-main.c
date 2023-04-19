0bff1fadd8879588171717ebd1af121491ac37c1
devcom22 <mouad.elb06@gmail.com>
2023-04-19 16:54:16 -0700

    3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,13 @@
-
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
 /**
+ * main - main function
+ * @argc: integer argument
+ * @argv: char argument
+ * Return: integer
  */
-int main(void)
+int main(int argc, char *argv[])
 {
+	
 }
