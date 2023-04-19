595b019708afc3103b26ae67f2891cceba1e7710
devcom22 <mouad.elb06@gmail.com>
2023-04-19 10:56:13 -0700

    add a function to the 1-array_iterator.c file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,9 +8,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-
-	for (i = 0; i < size; i++)
+	
+	if (action && array)
 	{
-		action(*(array + i));
+		for (i = 0; i < size; i++)
+		{
+			action(*(array + i));
+		}
 	}
 }
