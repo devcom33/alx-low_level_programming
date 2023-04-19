02a3e02242ea316428b32f1ca1fe83b3ec81f68e
devcom22 <mouad.elb06@gmail.com>
2023-04-19 15:03:38 -0700

    write all functions in the 3-op_functions.c file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - add two arguments
+ * @a: integer argument
+ * @b: integer argument
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub two arguments
+ * @a: integer argument
+ * @b: integer argument
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul of two arguments
+ * @a: integer argument
+ * @b: integer argument
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div of two arguments
+ * @a: integer argument
+ * @b: integer argument
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod of two arguments
+ * @a: integer argument
+ * @b: integer argument
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
