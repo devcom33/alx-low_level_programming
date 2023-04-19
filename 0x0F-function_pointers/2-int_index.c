1bbe027a3ec2ccfcf242da43852c9d992fa11a64
devcom22 <mouad.elb06@gmail.com>
2023-04-19 11:14:45 -0700

    add a function to the 2-int_index.c file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array argument
+ * @size: size of the array
+ * @cmp: compare values
+ * Return: the index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if ((size <= 0) || (!array) || (!cmp))
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + i)))
+		{
+			return (i);
+		}
+	}
+	return (0);
+}
