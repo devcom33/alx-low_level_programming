f4dfe8b46f900328870bef862689c038b3b7c965
devcom22 <mouad.elb06@gmail.com>
2023-04-19 16:19:23 -0700

    add code to 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,14 +1,29 @@
 #include "3-calc.h"
+#include <stdio.h>
 /**
+ * get_op_func - perform the operation
+ * @s: string argument
+ * Return: integer
  */
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-
-
-
-
-
-
-
+	while (i < 6)
+	{
+		i++;
+		if (s == ops->op)
+		{
+			return (ops->f);
+		}
+	}
+	return (NULL);
 }
