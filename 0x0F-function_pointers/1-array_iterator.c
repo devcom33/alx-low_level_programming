620941a3c3e10fd63345cd070da0ee5db2d49f49
devcom22 <mouad.elb06@gmail.com>
2023-04-19 10:52:46 -0700

    add a function to the 1-array_iterator.c file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - print array elements in decimal and hex
+ * @array: array argument
+ * @size: size of the array
+ * @action: action function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(*(array + i));
+	}
+}
