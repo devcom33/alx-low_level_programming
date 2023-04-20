9f33665da6e8bb1d681771b008209e304f21fb4a
devcom22 <mouad.elb06@gmail.com>
2023-04-19 18:03:33 -0700

    add condition to the 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,8 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
+	if (argc != 4)
+	{
+		printf("Error");
+		return (1);
+	}
 	int nm1 = atoi(argv[1]);
-	char op = argv[2];
+	char *op = argv[2];
 	int nm2 = atoi(argv[2]);
 	return (get_op_func(op)(nm1, nm2));
 }
