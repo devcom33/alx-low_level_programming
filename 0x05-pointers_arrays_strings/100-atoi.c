abe330000fd06c67a069c7fa9d52db966bb4fa37
devcom22 <mouad.elb06@gmail.com>
2023-04-25 04:16:28 -0700

    solving some problems

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@ int _atoi(char *s)
 {
 	int i = 0, result = 0, op = 1;
 
-	while (((s[i] == '-') || (s[i] == '+')) && (s[i] != '\0'))
+	while (((s[i] - '0' >= 0) && (s[i] - '0' <= 9)) && (s[i] != '\0'))
 	{
 		if (s[i] == '-')
 		{
@@ -18,7 +18,10 @@ int _atoi(char *s)
 	}
 	for (; s[i] != '\0'; i++)
 	{
-		result = result * 10 + s[i] - '0';
+		if (((s[i] - '0' >= 0) && (s[i] - '0' <= 9)))
+		{
+			result = result * 10 + s[i] - '0';
+		}
 	}
 	return (result * op);
 }
