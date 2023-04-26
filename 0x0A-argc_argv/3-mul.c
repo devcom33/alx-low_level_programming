e8a6d16a8b654dc058b9ecff8117b35833e3cd5d
devcom22 <mouad.elb06@gmail.com>
2023-04-26 11:48:11 -0700

    add _atoi function

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 #include "main.h"
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string argument
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	unsigned int i = 0, result = 0, op = 1;
+
+	while (((s[i] - '0' < 0) || (s[i] - '0' > 9)) && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+		{
+			op *= -1;
+		}
+		i++;
+	}
+	for (; s[i] != '\0'; i++)
+	{
+		if (((s[i] - '0' >= 0) && (s[i] - '0' <= 9)))
+		{
+			result = result * 10 + s[i] - '0';
+			if ((s[i + 1] - '0' < 0) || (s[i + 1] - '0' > 9))
+			{
+				break;
+			}
+		}
+	}
+	return (result * op);
+}
 /**
  * main - print program name
  * @argc: argument counter
@@ -8,7 +39,14 @@
  */
 int main(int argc, char **argv)
 {
-	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	if (argc != 3)
+	{
+		printf("%s", Error);
+	}
+	else
+	{
+		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+	}
 	(void)argc;
 	return (0);
 }
