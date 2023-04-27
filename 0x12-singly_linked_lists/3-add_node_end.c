8b58543aa98d7f15a31bf2bd1ac8fb680874a7e7
devcom22 <mouad.elb06@gmail.com>
2023-04-27 16:14:06 -0700

    add node from the end of LL

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+/**
+ * _strlen - length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * add_node_end - add nodes in the end
+ * @head: head
+ * @str: data
+ * Return: list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp;
+	list_t *tmp2;
+	int len = 0;
+
+	if (!tmp || !str)
+	{
+		return (NULL);
+	}
+	tmp->str = strdup(str);
+	if (!tmp->str)
+	{
+		free(tmp);
+		return (NULL);
+	}
+	tmp->len = _strlen(tmp->str);
+	tmp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return (tmp);
+	}
+	tmp2 = *head;
+	while (!tmp2->next)
+	{
+		tmp2 = tmp2->next;
+	}
+	tmp2->next = tmp;
+	return (tmp);
+}
