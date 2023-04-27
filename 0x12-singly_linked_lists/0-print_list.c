61076b6302790d1950aec37431e09277d437f08e
devcom22 <mouad.elb06@gmail.com>
2023-04-27 12:24:45 -0700

    resolve issues in the print_list file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t sz = 0;
-
+	
 	while (h)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] (%s)\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		sz++;
 		h = h->next;
