1d2b3787b889e105d609c5c90e9d27e4782cfd10
devcom33 <mouad.elb06@gmail.com>
2023-04-29 05:04:52 -0700

    handle errors

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,15 +10,18 @@
 int main(int argc, char *argv[])
 {
 	int nm1;
-	char *op = argv[2];
-	int nm2 = atoi(argv[3]);
-	int (*op_func)(int, int) = get_op_func(op);
+	char *op;
+	int nm2;
+	int (*op_func)(int, int);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	op = argv[2];
+	op_func = get_op_func(op);
 	nm1 = atoi(argv[1]);
+	nm2 = atoi(argv[3]);
 	printf("%d\n", op_func(nm1, nm2));
 	return (0);
 }
