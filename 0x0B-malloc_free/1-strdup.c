eff8dd8ccb8be6ff2dd0340a12be54902fe4cb81
devcom33 <mouad.elb06@gmail.com>
2023-04-30 14:26:35 -0700

    add _strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strdup - copy of the string
+ * @str: string argument
+ * Return: a duplcate string
+ */
+char *_strdup(char *str)
+{
+	int len = 0, i;
+	char *dup;
+	char *fld = "failed to allocate memory";
+
+	while(*(str + len) != '\0')
+	{
+		len++;
+	}
+	dup = malloc(sizeof(char) * len);
+	if (dup == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		*(dup + i) = *(str + i);
+	}
+	return (dup);
+	free(dup);
+}
