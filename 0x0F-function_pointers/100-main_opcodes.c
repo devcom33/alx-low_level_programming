a36213e2283af21f2846ffd61d950377b0bee4f4
devcom33 <mouad.elb06@gmail.com>
2023-05-01 11:43:05 -0700

    main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -25,7 +25,10 @@ int main(int argc, char *argv[])
 	for (i = 0; i < bytes; i++)
 	{
 		if ((bytes - 1) == i)
+		{
 			printf("%02hhx\n", *(bt + i));
+			break;
+		}
 		printf("%02hhx", *(bt + i));
 	}
 	return (0);
