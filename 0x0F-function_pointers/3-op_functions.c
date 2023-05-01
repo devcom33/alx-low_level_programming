4ede35b51ad999cd97863db1c750425dde1e5c5f
devcom33 <mouad.elb06@gmail.com>
2023-05-01 06:15:17 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -41,6 +41,11 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+        {
+                printf("Error\n");
+                exit(100);
+        }
 	return (a / b);
 }
 
@@ -55,7 +60,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("%s\n", "Error");
-		return (100);
+		exit(100);
 	}
 	return (a % b);
 }
