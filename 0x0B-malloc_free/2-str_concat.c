c2f2bae54b45b1d781c09f1ca145a0ce681b4324
devcom33 <mouad.elb06@gmail.com>
2023-05-01 02:08:09 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,11 @@ char *str_concat(char *s1, char *s2)
 	char *fld = "failed to allocate memory";
 
 	if (s1 == NULL && s2 == NULL)
-	{
 		return (NULL);
-	}
+	if (s1 == NULL && s2 != NULL)
+		return (s2);
+	if (s1 != NULL && s2 == NULL)
+		return (s1);
 	while (*(s1 + len1) != '\0')
 		len1++;
 	while (*(s2 + len2) != '\0')
