41eb93e6c56e352029962609eca9498459c78d4c
devcom33 <mouad.elb06@gmail.com>
2023-05-02 11:28:13 -0700

    add codes

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * cntword - count words
+ * @str: string argument
+ * Return: number of words
+ */
+int cntword(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		if (str[i] == ' ')
+			str++;
+		else
+		{
+			while (*str != '\0' && *str != ' ')
+				str++;
+			len++;
+		}
+	}
+	return (len);
+}
+/**
+ * strtow - split a string into words
+ * @str: string argument
+ * Return: array of words
+ */
+char **strtow(char *str)
+{
+	int i, j, len;
+	string **nw;
+
+	if (str == NULL || *str == "")
+		return (NULL);
+	len = cntword(str);
+	nw = malloc((len + 1) * sizeof(char *));
+	if (nw == NULL || len == 0)
+		return (NULL);
+	return (nw);
+}
