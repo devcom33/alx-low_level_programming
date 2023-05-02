dc7eb1bebd33450e1058bd77630ca3ddeb92297e
devcom33 <mouad.elb06@gmail.com>
2023-05-02 04:35:26 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * argstostr -  function that concatenates all the arguments
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: arguments or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 1; i < ac; i++)
+	{
+		return (av[i]);
+	}
+	return (NULL);
+}
