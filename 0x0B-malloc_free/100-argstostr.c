c978a89e482a51ee1761e0c23a3c7adffbc1857f
devcom33 <mouad.elb06@gmail.com>
2023-05-02 04:38:01 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,12 +8,12 @@
 char *argstostr(int ac, char **av)
 {
 	int i;
-
+	char *c = "\n";
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (i = 1; i < ac; i++)
 	{
-		return (av[i]);
+		return (av[i] + c);
 	}
 	return (NULL);
 }
