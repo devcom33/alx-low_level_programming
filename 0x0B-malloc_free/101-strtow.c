097aba4dc3331ca9fe777c7e9a5c981c074dcb8d
devcom33 <mouad.elb06@gmail.com>
2023-05-02 11:30:58 -0700

    add codes

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,10 +28,10 @@ int cntword(char *str)
  */
 char **strtow(char *str)
 {
-	int i, j, len;
+	int len;
 	char **nw;
 
-	if (str == NULL || *str == '')
+	if (str == NULL || *str == "")
 		return (NULL);
 	len = cntword(str);
 	nw = malloc((len + 1) * sizeof(char *));
