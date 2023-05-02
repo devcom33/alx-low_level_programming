20c50cc5acacc6d72b92f24c0e3c6116aab9dc45
devcom33 <mouad.elb06@gmail.com>
2023-05-02 15:41:43 -0700

    free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 /**
+ * free_listint - free LL
+ * @head: head of LL
  */
 void free_listint(listint_t *head)
 {
-	listint_t tmp = head;
-
+	listint_t *tmp = head;
+	
+	if(!head)
+	{
+		free(tmp);
+		free(head);
+	}
 	while (head->next)
 	{
 		head = tmp->next;
