92fee3001a1cbed0aa5a2edf35f6508b008745fa
devcom33 <mouad.elb06@gmail.com>
2023-05-02 14:48:59 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
  * *add_nodeint - adds a new node at the beginning of a LL
- * @h: a node
+ * @head: head of LL
+ * @n: value of the node
  * Return: LL
  */
 listint_t *add_nodeint(listint_t **head, const int n)
