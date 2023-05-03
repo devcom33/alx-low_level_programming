1b2647a899924c55cc77afe5f9df926ba8541e04
devcom33 <mouad.elb06@gmail.com>
2023-05-02 17:44:26 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
  * sum_listint - the sum of all the data
- * @head - head of LL
+ * @head: head of he LL
  * Return: sum of LL
  */
 int sum_listint(listint_t *head)
 {
 	int s = 0;
 
-	while (!head)
+	while (head != NULL)
 	{
 		s += head->n;
 		head = head->next;
