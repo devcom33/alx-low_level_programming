6e15c0f238df50d700da3a5286cbe28c08c9f1be
devcom33 <mouad.elb06@gmail.com>
2023-05-03 11:56:15 -0700

    add _strlen

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * _strlen - length of a string
+ * @s: string
+ * Return: length
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
  * string_nconcat - concat two string
  * @s1: first string
  * @s2: second string
@@ -11,14 +26,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int i = 0, j = 0;
 
-	s = malloc((n * 2) + 2);
+	if (_strlen(s2) <= n)
+		n = _strlen(s2);
+	s = malloc((n + _strlen(s1)) + 1);
 	if (!s)
 		return (NULL);
 	if (!s1)
 		s1 = "";
 	if (!s2)
 		s2 = "";
-	for (i = 0; s1[i] != '\0' && i < n; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s[i] = s1[i];
 	}
@@ -28,6 +45,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	s[i + 1] = '\0';
-	s[i + 2] = '\n';
 	return (s);
 }
