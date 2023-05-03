1886113abbad2848e367821edb3ed36fd709aae2
devcom33 <mouad.elb06@gmail.com>
2023-05-03 09:01:43 -0700

    new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,43 @@
 #include "dog.h"
 /**
+ * _strdup - copy of the string
+ * @str: string argument
+ * Return: a duplcate string
+ */
+char *_strdup(char *str)
+{
+	int len = 0, i;
+	char *dup;
+	char *fld = "failed to allocate memory";
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	len++;
+	dup = malloc(sizeof(char) * len);
+	if (dup == NULL)
+	{
+		while ((*fld) != '\0')
+		{
+			_putchar(*fld);
+			fld++;
+		}
+		_putchar('\n');
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		*(dup + i) = *(str + i);
+	}
+	return (dup);
+	free(dup);
+}
+/**
  * new_dog - create a new dog
  * @name: name of the dog
  * @age: name of the age
