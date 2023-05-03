017473ca128f05ad98391b9f705acbed04fe3f1b
devcom33 <mouad.elb06@gmail.com>
2023-05-03 13:31:02 -0700

    add func to the 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum all arguments
+ * @n: num of args
+ * Return: sum of args
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	return (sum);
+}
