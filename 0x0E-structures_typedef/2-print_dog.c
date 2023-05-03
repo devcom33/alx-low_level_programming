1189395f58dd00639365d2662bf5d0d56cea63bc
devcom33 <mouad.elb06@gmail.com>
2023-05-03 08:07:51 -0700

    create a function that print dog info

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
 	if (!(d->age))
-		printf("Age: (nil)\n");
+		printf("Age: %f\n", 0.0000);
 	else
 		printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
