8622d49a5bb94d4512024687a51d96cae6e3cd0e
devcom33 <mouad.elb06@gmail.com>
2023-05-03 12:10:46 -0700

    setup calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: nmemb argument
+ * @size: size argument
+ * Return: a pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+
+
+
+
+
+
+
+
+
+}
