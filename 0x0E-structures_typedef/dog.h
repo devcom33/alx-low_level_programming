033c57ebb30db079764045865179249f051df193
devcom33 <mouad.elb06@gmail.com>
2023-05-03 06:21:13 -0700

    add libs to the dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dog - structure for a dog
  * @name: name of the dog
@@ -13,7 +15,6 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
