0b96c3d98a78adf1e0d6e56d628fb129b6edc4ec
devcom33 <mouad.elb06@gmail.com>
2023-05-03 08:54:07 -0700

    new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,6 +4,7 @@
  * @name: name of the dog
  * @age: name of the age
  * @owner: name of the owner
+ * Return: new dog created
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -11,8 +12,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
+	newdog->name = _strdup(name);
+	if (!(newdog->name))
+	{
+		free(newdog);
+		return (NULL);
+	}
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = _strdup(owner);
+	if (!(newdog->owner))
+	{
+		free(newdog);
+		return (NULL);
+	}
 	return (newdog);
 }
