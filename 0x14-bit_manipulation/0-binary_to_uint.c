4cd6076c0b96dc624e9cf0001178fd0b46271f89
devcom33 <mouad.elb06@gmail.com>
2023-05-06 06:28:37 -0700

    create a function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,17 +1,38 @@
 #include "main.h"
 /**
+ * _strlen - length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary number in char
+ * Return: decimal
  */
 unsigned int binary_to_uint(const char *b)
 {
-	
+	unsigned int c, n = 1, i;
 
-
-
-
-
-
-
-
-
-
+	if (!b)
+		return (0);
+	while (b)
+		b++;
+	b--;
+	for (i = 0; i < _len(b); i++)
+	{
+		c = c + 2 * n * (*b - '0');
+		b--;
+		n = 2;
+	}
+	return (c);
 }
