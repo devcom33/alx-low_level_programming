92da25be784b47f4531c54fa159684d1f74ccb2a
devcom33 <mouad.elb06@gmail.com>
2023-05-07 08:31:55 -0700

    fix errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,20 +7,25 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int n_ = n, c = 1, len = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	while (n_ > 0)
 	{
 		len++;
-		n = n>>1;
+		n_ = n_>>1;
 	}
 	len -= 1;
 	if (len > 0)
-		c = c<<len;
-	while (n > 0)
+		c = c << len;
+	while (c > 0)
 	{
 		if (c & n)
 			_putchar('1');
 		else
 			_putchar('0');
-		n = n>>1;
+		c = c>>1;
 	}
 }
