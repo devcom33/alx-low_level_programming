60356b0704d1a1c46e31baa717d8cee54d450d83
devcom33 <mouad.elb06@gmail.com>
2023-05-07 04:05:07 -0700

    fix some errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,15 @@
 #include "main.h"
 /**
- * _strlen - length of a string
- * @s: string
- * Return: length
- */
-unsigned int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: binary number in char
  * Return: decimal
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int c = 0, n = 1, k = 1;
+	unsigned int c = 0;
 
 	if (!b)
 		return (0);
-	while (k < _strlen(b))
-	{
-		n = n * 2;
-		k++;
-	}
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
@@ -38,7 +18,6 @@ unsigned int binary_to_uint(const char *b)
 		{
 			c = c + n * (*b - '0');
 			b++;
-			n /= 2;
 		}
 	}
 	return (c);
