607d0ee333533070cb657dc37f3f841c7438f386
devcom33 <mouad.elb06@gmail.com>
2023-05-08 12:49:43 -0700

    fix errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,12 +8,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	char buffer[1] = text_content;
+	char *buffer = malloc(sizeof(text_content));
 	ssize_t wr;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, 600);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	wr = write(fd, buffer, 1);
