4ffe947ae4018b2d91b1495b98308f007dc88da2
devcom33 <mouad.elb06@gmail.com>
2023-05-08 13:14:05 -0700

    write nothing to the file condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		return (1);
 	while (text_content[len])
 		len++;
 	wr = write(fd, text_content, len);
