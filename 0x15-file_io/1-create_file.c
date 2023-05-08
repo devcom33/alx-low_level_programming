3be90b0e35a677ebc787a13b4dc29469909a541d
devcom33 <mouad.elb06@gmail.com>
2023-05-08 12:17:47 -0700

    add codes

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content: content of the file
+ * Return: return number
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	char buffer[1] = text_content;
+	ssize_t wr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY, 600);
+	if (fd == -1)
+		return (-1);
+	wr = write(fd, buffer, 1);
+	if (wr < 0)
+		return (-1);
+	return (wr);
+}
