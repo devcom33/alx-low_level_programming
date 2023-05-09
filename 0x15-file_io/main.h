8524c3808767c518ea600ef7a5aa4d0fbed7c4f5
devcom33 <mouad.elb06@gmail.com>
2023-05-09 10:58:12 -0700

    chng

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int cp_file(char *file_from, char *file_to);
+void cp_file(char *file_from, char *file_to);
 #endif
