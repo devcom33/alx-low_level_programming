ccb48c6c33789994f9a4b98ed88b1955217bf6e8
devcom33 <mouad.elb06@gmail.com>
2023-05-20 16:45:15 -0700

    add clear_bit fun

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - clear bit function
+ * @n: bits arg
+ * @index: index argument
+ * Return: Return value
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int c = 0 << index;
+
+	if (index > 64)
+		return (-1);
+	(*n) = c & (*n);
+	return (1);
+}
