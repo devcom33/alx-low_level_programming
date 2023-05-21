6784f7af46a88e4fa54a967a9b121e2a6f5373e5
devcom33 <mouad.elb06@gmail.com>
2023-05-21 09:05:13 -0700

    create a function

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * flip_bits - a function that returns the number of bits
+ * @n: first unsi integer argument
+ * @m: second integer argument
+ * Return: flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int v = n & m;
+	size_t i;
+	unsigned int cnt = 0;
+
+	while (v != 0)
+	{
+		if (v | 1 == v)
+		{
+			v = v>>1;
+		}
+		else if (v | 1 != v)
+		{
+			cnt++;
+			v = v>>1;
+		}
+	}
+	return (cnt);
+}
