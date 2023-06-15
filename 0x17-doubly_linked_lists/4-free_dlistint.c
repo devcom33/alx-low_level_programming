633b947cedd7c4fde709e691b66d41abcfdf3af0
devcom33 <mouad.elb06@gmail.com>
2023-06-15 14:07:35 -0700

    free DLL

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+	dlistint_t *tmp2 = NULL;
+
+	if (head != NULL)
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp2 = tmp->prev;
+		while (tmp2->prev)
+		{
+			free(tmp);
+			tmp = tmp2;
+			tmp2 = tmp2->prev;
+		}
+	}
+	free(tmp);
+	free(head);
+}
