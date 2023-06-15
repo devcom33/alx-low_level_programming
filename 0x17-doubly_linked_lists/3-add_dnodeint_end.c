99d04f2e3b34cc6320ae9c6416adaa0c8301de24
devcom33 <mouad.elb06@gmail.com>
2023-06-15 13:32:10 -0700

    add node int

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 /**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: head of DLL
+ * @n: data
+ * Return: new node
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
