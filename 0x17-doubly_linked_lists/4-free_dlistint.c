fdcbeb6d65b8425d196026bd283c575f163bddcf
devcom33 <mouad.elb06@gmail.com>
2023-06-15 14:26:24 -0700

    dlistint

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,18 +8,10 @@ void free_dlistint(dlistint_t *head)
 	dlistint_t *tmp = head;
 	dlistint_t *tmp2 = NULL;
 
-	if (head != NULL)
+	while (tmp)
 	{
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp2 = tmp->prev;
-		while (tmp2->prev)
-		{
-			free(tmp);
-			tmp = tmp2;
-			tmp2 = tmp2->prev;
-		}
+		tmp2 = tmp->next;
+		free(tmp);
+		tmp = tmp2;
 	}
-	free(tmp);
-	free(head);
 }
