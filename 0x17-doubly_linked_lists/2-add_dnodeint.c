7dd9f21259698b6b68ded99abdc72ff1f87218a2
devcom33 <mouad.elb06@gmail.com>
2023-06-15 11:09:44 -0700

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 /**
+ * add_dnodeint - adds a new node at the beginning
+ * @head: head of the DLL
+ * @n: data
+ * Return: new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
