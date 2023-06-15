29eb48e6f3279eb8a4d2862399ec69d6632ca421
devcom33 <mouad.elb06@gmail.com>
2023-06-15 11:11:54 -0700

    fix style errors

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *tmp = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (tmp == NULL)
-		return NULL;
+		return (NULL);
 	tmp->n = n;
 	tmp->prev = NULL;
 	tmp->next = (*head);
