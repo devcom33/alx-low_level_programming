cf17914f9dec62682fcfc710adea141f15cd899b
devcom33 <mouad.elb06@gmail.com>
2023-07-08 06:31:19 -0700

    fix betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* table;
+	hash_table_t *table;
 	size_t i;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * table->size);
 	for (i = 0; i < size; i++)
 	{
-		table->array[i] = NULL;	
+		table->array[i] = NULL;
 	}
 	return (table);
 }
