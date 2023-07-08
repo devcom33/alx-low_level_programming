47ea597a49072ea019727d5f82e34dc122989413
devcom33 <mouad.elb06@gmail.com>
2023-07-08 06:07:29 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,21 +1,23 @@
 #include "hash_tables.h"
 /**
+ * hash_table_create - create a hash table
+ * @size: size of hash table
+ * Return: ...
  */
 
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	struct hash_table_s *table;
+	size_t i;
 
-
-
-
-
-
-
-
-
-
-
-
-
+	table = (struct hash_table_s*)malloc(sizeof(struct hash_table_s));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;	
+	}
+	return (table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,7 @@
 #ifndef HASH_TABLES
 #define HASH_TABLES
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
