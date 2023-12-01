98752ccc0d87ec9c7d3cd956ce69885fa810dfda
devcom33 <mouad.elb06@gmail.com>
2023-12-01 06:02:40 -0800

    solve some issues

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,10 +6,11 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_s* table = (hash_table_s*) malloc(sizeof(hash_table_s));
+	hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
+	size_t i;
 
 	table->size = size;
-	table->array = (hash_node_t**) malloc(table->size * sizeof(hash_node_s*));
+	table->array = (hash_node_t**) malloc(table->size * sizeof(hash_node_t*));
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
