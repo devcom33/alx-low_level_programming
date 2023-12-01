6e2c08526644ac33ac159bfe7c56398140ea37a4
devcom33 <mouad.elb06@gmail.com>
2023-12-01 06:08:22 -0800

    solving styles errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,19 +1,19 @@
 #include "hash_tables.h"
 /**
- * hash_table_s - create a hash table
+ * hash_table_create - create a hash table
  * @size: size of hash table
  * Return: hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	size_t i;
 
 	table->size = size;
-	table->array = (hash_node_t**) malloc(table->size * sizeof(hash_node_t*));
+	table->array = (hash_node_t **) malloc(table->size * sizeof(hash_node_t *));
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
-	return table;
+	return (table);
 }
