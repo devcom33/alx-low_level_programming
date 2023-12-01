00ff7eb64cc5216ccf4d7364793bdfc1f92f4f5a
devcom33 <mouad.elb06@gmail.com>
2023-12-01 06:33:53 -0800

    changing table_size to size in alloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t **) malloc(table->size * sizeof(hash_node_t *));
+	table->array = (hash_node_t **) malloc(size * sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
