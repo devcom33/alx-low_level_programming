b4417c87176116019c812325a3315a863abf9e13
devcom33 <mouad.elb06@gmail.com>
2023-12-02 10:11:18 -0800

    first version

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - add an element to the hash table
+ * @ht: hash table
+ * @key: key of an elem
+ * @value: value of the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	if (key == NULL || ht == NULL)
+		return (0);
+
+	return (1);
+}
