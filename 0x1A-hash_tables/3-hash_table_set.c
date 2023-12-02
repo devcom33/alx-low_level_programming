39c75ca9cda8830f211682fba0e2a82c00bb5baa
devcom33 <mouad.elb06@gmail.com>
2023-12-02 15:05:06 -0800

    add new to 3 funct

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,9 +8,19 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	value = value;
-	if (key == NULL || ht == NULL)
+	int index;
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (key == NULL || ht == NULL || node == NULL)
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node;
+	}
 	return (1);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,7 @@
 #ifndef HASH_TABLES
 #define HASH_TABLES
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -37,6 +38,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-
 
 #endif
